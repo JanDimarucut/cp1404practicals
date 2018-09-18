49851c3d8507d1d105f028937412bb5c3948c931
JanDimarucut <jan.dimarucut@my.jcu.edu.au>
2018-09-18 15:50:03 +1000

    Add handle button function

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -13,7 +13,7 @@ class BoxLayoutDemo(App):
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
     def handle_clear(self):
-        self.root.ids.output_label.text = ''
+        self.root.ids.output_label.text = 'Enter name'
         self.root.ids.input_name.text = ''
 
 
