fcc8554ef37ca1d66bdbe2769d5d56b285811fdb
JanDimarucut <jan.dimarucut@my.jcu.edu.au>
2018-09-18 11:03:11 +1000

    Removed a statement

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -18,13 +18,12 @@ def main():
     #    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     for i, guitar in enumerate(guitars):
-        vintage_string = ''
         if guitar.is_vintage():
             vintage_string = "(vintage)"
             print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}"
                   "{2}".format(i + 1, guitar, vintage_string))
         else:
-            print("No guitars")
+            print("No vintage guitars")
 
 
 main()
