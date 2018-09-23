25fbc34d26b0042ac10fe91d588ebbe033b1a986
JanDimarucut <jan.dimarucut@my.jcu.edu.au>
2018-09-23 20:07:24 +1000

    Add function and modified

--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -9,14 +9,12 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
-
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (400, 200)
+        Window.size = (200, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
