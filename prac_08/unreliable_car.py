d3108db4654eeb1a2d706a31bbb4c0decd081ebe
JanDimarucut <jan.dimarucut@my.jcu.edu.au>
2018-10-02 15:44:04 +1000

    Add taxi and unreliable car program

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,18 @@
+from prac_06.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        return super().__str__()
+
+    def drive(self, distance):
+        random_number = randint(1, 100)
+        if random_number >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,11 @@
+from prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    my_car = UnreliableCar("Decent car", 100, 70)
+    my_car.drive(50)
+    print(my_car)
+
+
+
+main()
